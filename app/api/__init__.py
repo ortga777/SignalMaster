cb79f69ad34496ed1d3612607da5f7d4e3059cf0
ortga777 <jossebastiao472@gmail.com>
2025-10-21 22:36:58 +0100

    Refactor import statements in __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter
-from app.api.auth_router import router as auth_router
-from app.api.signals_router import router as signals_router
-from app.api.admin_router import router as admin_router
-from app.api.brokers_router import router as broker_router
-from app.api.ml_router import router as ml_router
-# Note: these files are created above
+from .auth_router import router as auth_router
+from .signals_router import router as signals_router
+from .brokers_router import router as broker_router  # Note: brokers_router (com 's')
+from .ml_router import router as ml_router
+from .admin_router import router as admin_router
