13b39cc108fcb33538853a1ed6153f0bd0d02512
ortga777 <jossebastiao472@gmail.com>
2025-10-21 23:48:24 +0100

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,5 @@
 from .auth_router import router as auth_router
 from .signals_router import router as signals_router
-from .brokers_router import router as broker_router  # Note: brokers_router (com 's')
+from .brokers_router import router as broker_router
 from .ml_router import router as ml_router
 from .admin_router import router as admin_router
